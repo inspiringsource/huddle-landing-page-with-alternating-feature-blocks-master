652a4785c9c241564086d08ebf82d62309054b38
inspiringsource <ab.b2k3@gmail.com>
2021-12-12 13:48:04 +0100

    first deployment

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,13 @@
-<style>
+<style land="scss">
+    h1,
+    h2,
+    h3, 
+    p {
+        text-align: left;
+        margin: 3rem;
+        width: 26rem;
+    }
+  
     .grid{
         display: grid;
         /*grid-template-columns: repeat(3, minmax(200px, 1fr));*/
@@ -14,7 +23,6 @@
         grid-template-columns: repeat(2, auto);
         grid-column: 1/3;
         grid-row: 1/3;
-        background-color: orange;
         
         /* padding: 1em; */
         /*text-align: left;*/
@@ -28,17 +36,14 @@
         grid-gap: 1em;
         grid-column: 1/3;
         grid-row: 1/4; /*you can overlap grids*/
-        border: black solid 1px;;    
         z-index: -1;
         height: 100%;
     }
     .nav-1{
         padding: 1em;
-        border: 1px solid black;
     }
     .nav-2{
         padding: 1em;
-        border: 1px solid black;
         align-items: right;
         text-align: right;
         display: flex;
@@ -58,10 +63,13 @@
     text-align: center;
     text-decoration: none;
     color: rgba(8, 36, 39, 1);
-    box-shadow: 0 0 4px rgba(128, 128, 128, 0.267);
+    box-shadow: 0 0 4px rgba(128, 128, 128, 0.567);
     border-color: white;
 
     margin-left: auto;
+    }
+    .try-btn:hover{
+        box-shadow: 0 0 4px rgba(128, 128, 128, 0.167);
     }
     
     .grid > div{
@@ -77,6 +85,9 @@
         text-align: center;
         min-width: 100vw;
         margin-left: calc(50% - 50vw);
+    }
+    .main-title{
+        width: 21rem;
     }
     .Box-2 > div{
         display: flex;
@@ -113,6 +124,11 @@
     margin-left: auto;
     margin-right: auto;
     }
+    .start-btn:hover{
+    
+        opacity: 65%;
+        box-shadow: 0 0 25px rgba(36, 36, 36, 0.9);
+    }
     .footer-btn{
         background-color: hsl(322, 100%, 66%);
         border-radius: 28px;
@@ -125,7 +141,7 @@
         text-align: center;
         text-decoration: none;
         color: rgba(8, 36, 39, 1);
-        box-shadow: 0 0 5px rgba(128, 128, 128, 0.541);
+        box-shadow: 0 0 12px rgba(36, 36, 36, 0.767);
         border-color: white;
         color: white;
 
@@ -133,21 +149,28 @@
         margin-right: auto;
         margin-top: -20px;
     }
+    .footer-btn:hover{
+        opacity: 65%;
+        box-shadow: 0 0 25px rgba(36, 36, 36, 0.9);
+    }
     .Box-1{
 
-        background-color: blueviolet !important;
-        
-        grid-column: 1/3;  
+        grid-column: 1/3;
+        background-color: white !important;  
        
     }
     .Box-1 > div{
 
         display: flex;
-        background-color: #eee;
-        border: 1px solid #ccc;
+
         grid-gap: 1em;
         text-align: center;
         justify-content: space-around;
+        margin-top: 1rem;
+        box-shadow: 0 0 5px rgba(128, 128, 128, 0.2);
+        border-radius: 28px;
+        background-color: rgb(255, 255, 255);
+        border: 1px solid rgba(204, 204, 204, 0.534);
     }
     .main-img {
     width: 40%;
@@ -161,11 +184,15 @@
 
     .Box-14{
         grid-column: 1/3;
-        grid-row: 7/10;
+        grid-row: 5/10;
         z-index: 10;
         width: 470px;
         height: 140px;
         justify-self: center;
+        box-shadow: 0 0 5px rgba(128, 128, 128, 0.2);
+        border-radius: 28px;
+        background-color: rgb(255, 255, 255) !important;
+        border: 1px solid rgba(204, 204, 204, 0.534);
     }
     .Box-14 h3{
         text-align: center;
@@ -204,11 +231,22 @@
         height: 300px;
         /*width: 100%; */
         color: white;
+        margin-top: 100px;
+        padding: 2.5rem;
+
 
     }
+
     .footerLogo{
         -webkit-filter: brightness(0) invert(1);
         filter: brightness(0) invert(1); 
+        padding: 1rem;
+    }
+    address {
+        padding: 0.6rem;
+    }
+    .contacInfo{
+        padding: 0.8rem;
     }
     .midFooter{
         display: flex;
@@ -240,6 +278,7 @@
 
     }
 
+    
     .footerinfolist2 li{
         display: inline;
         margin-left: 1rem;  
@@ -252,10 +291,14 @@
     .footerinfolist2 a{
         color:inherit;
     }
+    .attribution{
+        margin-top: 2rem;
+    }
+    
 </style>
 
 <div class="grid">
-    <div class="header-bg">Box 13</div>
+    <div class="header-bg"></div>
     <nav>
         <div class="nav-1">
             <img src="logo.svg" alt="header logo">
@@ -304,10 +347,7 @@
                 <img class="users-img main-img" src="./illustration-your-users.svg" alt="Your-Users">
             </div>
         </div>
-        <div>
-            <div class="Box-9">Box 9</div>
-            <div class="Box-10">Box 10</div>
-        </div>
+
     </div>
          
     
@@ -319,13 +359,13 @@
         <a class="footer-btn" href="www.frontendmentor.io">Get Started For Free</a>
     </div>
     <div class="Box-15">
-        <div> <img class="footerLogo" src="logo.svg" alt="footer logo">
+        <div class="leftFooter"> <img class="footerLogo" src="logo.svg" alt="footer logo">
         
 
             <address>
                 <img class="icons" src="/icon-location.svg" alt="location">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</address>
-            <div>
+            <div class="contacInfo">
                 <img class="icons" src="/icon-phone.svg" 
             alt="phone">
             +1-543-123-4567</div>
@@ -354,11 +394,19 @@
                 <li><a href=/#><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20"><path d="M10 .4C4.698.4.4 4.698.4 10s4.298 9.6 9.6 9.6s9.6-4.298 9.6-9.6S15.302.4 10 .4zm3.905 7.864c.004.082.005.164.005.244c0 2.5-1.901 5.381-5.379 5.381a5.335 5.335 0 0 1-2.898-.85c.147.018.298.025.451.025c.886 0 1.701-.301 2.348-.809a1.895 1.895 0 0 1-1.766-1.312a1.9 1.9 0 0 0 .853-.033a1.892 1.892 0 0 1-1.517-1.854v-.023c.255.141.547.227.857.237a1.89 1.89 0 0 1-.585-2.526a5.376 5.376 0 0 0 3.897 1.977a1.891 1.891 0 0 1 3.222-1.725a3.797 3.797 0 0 0 1.2-.459a1.9 1.9 0 0 1-.831 1.047a3.799 3.799 0 0 0 1.086-.299a3.834 3.834 0 0 1-.943.979z" fill="currentColor"/></svg> </a></li>
 
 
-                <li><a href=/#><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20"><path d="M13 10a3 3 0 1 1-6 0c0-.171.018-.338.049-.5H6v3.997c0 .278.225.503.503.503h6.995a.503.503 0 0 0 .502-.503V9.5h-1.049c.031.162.049.329.049.5zm-3 2a2 2 0 1 0-.001-4.001A2 2 0 0 0 10 12zm2.4-4.1h1.199a.301.301 0 0 0 .301-.3V6.401a.301.301 0 0 0-.301-.301H12.4a.301.301 0 0 0-.301.301V7.6c.001.165.136.3.301.3zM10 .4A9.6 9.6 0 0 0 .4 10a9.6 9.6 0 0 0 9.6 9.6a9.6 9.6 0 0 0 9.6-9.6A9.6 9.6 0 0 0 10 .4zm5 13.489C15 14.5 14.5 15 13.889 15H6.111C5.5 15 5 14.5 5 13.889V6.111C5 5.5 5.5 5 6.111 5h7.778C14.5 5 15 5.5 15 6.111v7.778z" fill="currentColor"/></svg> </a></li>
+                <li><a href=/#><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20"><path d="M13 10a3 3 0 1 1-6 0c0-.171.018-.338.049-.5H6v3.997c0 .278.225.503.503.503h6.995a.503.503 0 0 0 .502-.503V9.5h-1.049c.031.162.049.329.049.5zm-3 2a2 2 0 1 0-.001-4.001A2 2 0 0 0 10 12zm2.4-4.1h1.199a.301.301 0 0 0 .301-.3V6.401a.301.301 0 0 0-.301-.301H12.4a.301.301 0 0 0-.301.301V7.6c.001.165.136.3.301.3zM10 .4A9.6 9.6 0 0 0 .4 10a9.6 9.6 0 0 0 9.6 9.6a9.6 9.6 0 0 0 9.6-9.6A9.6 9.6 0 0 0 10 .4zm5 13.489C15 14.5 14.5 15 13.889 15H6.111C5.5 15 5 14.5 5 13.889V6.111C5 5.5 5.5 5 6.111 5h7.778C14.5 5 15 5.5 15 6.111v7.778z" fill="currentColor"/></svg></a></li>
 
-                
+                <footer class="attribution">
+                    Challenge by
+                <a href="https://www.frontendmentor.io/challenges/huddle-landing-page-with-alternating-feature-blocks-5ca5f5981e82137ec91a5100/hub/huddle-landing-page-with-alternating-feature-blocks-Xjm83bc1q" target="_blank">
+                  Frontend Mentor
+                </a>
+                . Coded by
+                <a href="https://www.frontendmentor.io/profile/inspiringsource" target="_blank">Avi B</a>
+                </footer>
             </ul>
         </div>
 
     </div>
+
 </div>
